add590a6af61911ceaa3b2f8524ab0485424b9b3
Tiakon <1036615650@qq.com>
2018-04-20 18:58:15 +0800

    add handler interface

---
+++ bugskiller-spider-stream/src/main/java/cn/bugskiller/spiderstream/handler/ChaperHandler.java
@@ -0,0 +1,22 @@
+package cn.bugskiller.spiderstream.handler;
+
+import cn.bugskiller.spiderstream.entity.Chapter;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 章节处理器
+ *
+ * @author Tiakon
+ * 2018/4/19 16:24
+ */
+public interface ChaperHandler {
+    /**
+     * 根据小说的URl，得到小说的全部章节。
+     * @param url 小说的URl
+     * @return  List 小说的全部章节
+     * @throws  IOException  接收来自 PageUtils -> crawlerPage() 中的异常。
+     */
+    List<Chapter> getChaptersByURL(String url) throws IOException;
+}
