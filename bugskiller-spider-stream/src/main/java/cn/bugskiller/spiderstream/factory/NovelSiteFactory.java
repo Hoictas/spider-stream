9ae09f810247d129a0becdc6fca7e0fa1ab43abe
Tiakon <1036615650@qq.com>
2018-04-22 21:53:28 +0800

    update

---
+++ bugskiller-spider-stream/src/main/java/cn/bugskiller/spiderstream/factory/NovelSiteFactory.java
@@ -0,0 +1,58 @@
+package cn.bugskiller.spiderstream.factory;
+
+import cn.bugskiller.spiderstream.novels.NovelSiteEnum;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 生成可采集网站的信息工厂
+ *
+ * @author Tiakon
+ * 2018/4/22 20:22
+ */
+final public class NovelSiteFactory {
+    public static final Map<NovelSiteEnum, Map<String, String>> NOVEL_SITES_CONTEXT = new HashMap<>(64);
+
+    static {
+        initContext();
+    }
+
+    private NovelSiteFactory() {
+    }
+
+    private static void initContext() {
+        SAXReader reader = new SAXReader();
+        try {
+            Document document = reader.read(NovelSiteFactory.class.getClassLoader().getResourceAsStream("novel-sites.xml"));
+            Element rootElement = document.getRootElement();
+            List<Element> sites = rootElement.elements("site");
+            for (Element site : sites) {
+                Map<String, String> siteMap = new HashMap<>(16);
+                List<Element> elements = site.elements();
+                for (Element element : elements) {
+                    siteMap.put(element.getName(), element.getTextTrim());
+                }
+                NOVEL_SITES_CONTEXT.put(NovelSiteEnum.getNovelSiteByUrl(siteMap.get("url")), siteMap);
+            }
+        } catch (DocumentException e) {
+            throw new RuntimeException("NOVEL_SITES_CONTEXT,初始化失败");
+        }
+
+    }
+
+    /**
+     * 得到网站小说章节的解析规则和网页的编码信息
+     *
+     * @param novelSiteEnum : 传入一个 NovelSiteEnum 类型
+     * @return Map<String , String> : 得到含有网站小说章节的解析规则和网页的编码信息
+     */
+    public static Map<String, String> getNovelSitesContext(NovelSiteEnum novelSiteEnum) {
+        return NOVEL_SITES_CONTEXT.get(novelSiteEnum);
+    }
+}
---
+++ bugskiller-spider-stream/src/main/java/cn/bugskiller/spiderstream/novels/NovelSiteEnum.java
@@ -0,0 +1,67 @@
+package cn.bugskiller.spiderstream.novels;
+
+/**
+ * 已经被支持的小说网站枚举
+ *
+ * @author Tiakon
+ * 2018/4/22 19:03
+ */
+public enum NovelSiteEnum {
+
+    LaBi(1, "https://www.lbxs.com"),
+    DingDian(2, "https://www.x23us.com"),
+    BiQqGe(3, "http://www.biquge.com.tw"),
+    BiXia(4, "https://www.bixia.org");
+
+    private int id;
+    private String url;
+
+    NovelSiteEnum(int id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public static NovelSiteEnum getNovelSiteById(int id) {
+        switch (id) {
+            case 1:
+                return LaBi;
+            case 2:
+                return DingDian;
+            case 3:
+                return BiQqGe;
+            case 4:
+                return BiXia;
+            default:
+                throw new RuntimeException("id = " + id + " 不是被支持的小说网站！！！");
+        }
+    }
+
+    public static NovelSiteEnum getNovelSiteByUrl(String url) {
+        //        values()方法的作用就是获取枚举类中的所有变量
+        for (NovelSiteEnum novelSiteEnum : values()) {
+            if (url.contains(novelSiteEnum.getUrl())) {
+                return novelSiteEnum;
+            }
+        }
+        throw new RuntimeException("url = " + url + " 不是被支持的小说网站！！！");
+    }
+
+}
+
+
